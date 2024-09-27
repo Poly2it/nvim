5a2465568ffcc0f609816128e3f36ad14527c942
Poly2it <84731064+Poly2it@users.noreply.github.com>
2024-09-27 23:39:54 +0200

    Add configuration flake

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "My own Neovim flake";
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs";
+    };
+  };
+  outputs = { nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+    inherit (pkgs) lib;
+    neovim = pkgs.neovim.overrideAttrs (oldAttrs:
+    let
+      runtimeDependencies = with pkgs; [
+        git
+
+        tree-sitter
+        gcc
+
+        wl-clipboard
+        ripgrep
+
+        clang-tools
+        lua-language-server
+        nixd
+        basedpyright
+      ];
+    in
+    {
+      pname = "neovim-polybit";
+      nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ runtimeDependencies;
+      installPhase = (oldAttrs.installPhase or "") + lib.concatStringsSep "\n" (map (x: "ln -s ${x.out}/bin/* $out/bin") runtimeDependencies);
+      postFixup = ''
+        wrapProgram $out/bin/nvim --prefix PATH : ${pkgs.lib.makeBinPath runtimeDependencies}
+      '';
+    });
+  in
+  {
+    packages.x86_64-linux.default = neovim;
+    apps.x86_64-linux.default = {
+      type = "app";
+      program = "${neovim}/bin/nvim";
+      dependencies = with pkgs; [ gcc ];
+    };
+  };
+}
+
